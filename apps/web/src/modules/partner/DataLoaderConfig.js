e5b9d6386bd7fd400d09905849c18e3b9a437576
Pradeep K <Pradeep.K@precisely.com>
2024-03-12 13:23:42 +0530

    PEM-26667: Added locale files and sample list page

---
+++ apps/web/src/modules/partner/DataLoaderConfig.js
@@ -0,0 +1,19 @@
+import ListAPIHandler from '../../common/api-handler/list';
+
+const DataLoaderConfig = {
+  PARTNER: {
+    LIST: ({ getEnvironmentValue }) => {
+      return {
+        type: 'RESTAPI',
+        method: 'GET',
+        // url: getEnvironmentValue('USER.PERMISSIONTREE')
+        url: '/rest/sponsors/hsbc/partners',
+        handleOutput: (responseData) => {         
+          ListAPIHandler.handleResponse(responseData, 'partnerKey');
+        }
+      };
+    }
+  }
+};
+
+export default DataLoaderConfig;

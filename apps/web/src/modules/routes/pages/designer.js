198a1fba962bcb72d35f7f2c7a1eccc318badc8d
Ankit Kesari <Ankit.Kesari@precisely.com>
2024-03-16 20:57:44 +0530

    Tried to use page designer and component mapper

--- apps/web/src/modules/routes/pages/designer.js
+++ apps/web/src/modules/routes/pages/designer.js
@@ -10,7 +10,10 @@ import React from 'react';
 // Below line giving error
 //import * as Mapper from '@b2bi/carbon-mappers'
 //import * as Page from '@b2bi/page-designer'
+//import { PageDesigner }  from '@b2bi/page-designer'
+//import { componentMapper } from '@b2bi/carbon-mappers'
 
 export default function Designer() {
+  // return <PageDesigner componentMapper={componentMapper} />;
   return <div>Designer</div>;
 }

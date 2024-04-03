b8888ea7b3ec877e91763d969c597dadbb44dc27
Ankit Kesari <Ankit.Kesari@precisely.com>
2024-04-03 17:07:54 +0530

    Worked on the icons issue that was not showing in task tray

--- pem-ui/packages/flow-designer/src/assets/icons/custom.js
+++ pem-ui/packages/flow-designer/src/assets/icons/custom.js
@@ -1,9 +1,11 @@
-
+import React from 'react';
 
 export const CustomBlockIcon = () => {
+  return (
     <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-        <path d="M6.66699 6.66732H25.3337" stroke="black" stroke-linecap="round" />
-        <path d="M6.66699 25.3333H25.3337" stroke="black" stroke-linecap="round" />
-        <rect x="6.66699" y="12" width="18.6667" height="8" stroke="black" stroke-linejoin="round" />
+      <path d="M6.66699 6.66732H25.3337" stroke="black" stroke-linecap="round" />
+      <path d="M6.66699 25.3333H25.3337" stroke="black" stroke-linecap="round" />
+      <rect x="6.66699" y="12" width="18.6667" height="8" stroke="black" stroke-linejoin="round" />
     </svg>
-}
+  );
+};
--- pem-ui/packages/flow-designer/src/assets/icons/index.js
+++ pem-ui/packages/flow-designer/src/assets/icons/index.js
@@ -1,12 +1,9 @@
-import { CustomBlockIcon } from "./custom";
-import { SystemBlockIcon } from "./system";
-import { ApprovalBlockIcon } from "./approval";
-import { SponsorBlockIcon } from "./sponsor";
-import { PartnerBlockIcon } from "./partner";
-import { AttributeBlockIcon } from "./attribute";
-import { GatewayBlockIcon } from "./gateway";
+import { CustomBlockIcon } from './custom';
+import { SystemBlockIcon } from './system';
+import { ApprovalBlockIcon } from './approval';
+import { SponsorBlockIcon } from './sponsor';
+import { PartnerBlockIcon } from './partner';
+import { AttributeBlockIcon } from './attribute';
+import { GatewayBlockIcon } from './gateway';
 
-export {
-    PartnerBlockIcon,
-    CustomBlockIcon, SponsorBlockIcon, SystemBlockIcon, ApprovalBlockIcon, AttributeBlockIcon, GatewayBlockIcon
-}
+export { PartnerBlockIcon, CustomBlockIcon, SponsorBlockIcon, SystemBlockIcon, ApprovalBlockIcon, AttributeBlockIcon, GatewayBlockIcon };

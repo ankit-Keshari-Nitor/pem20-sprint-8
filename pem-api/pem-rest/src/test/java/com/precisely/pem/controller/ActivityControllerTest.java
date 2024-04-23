1dd70286c6d0cf75317cb8ba30800308f046c57c
shrirang.b <shrirang.b@precisely.com>
2024-04-23 18:01:07 +0530

    #26848 Junit Test cases for Create and Get API (Version,Definition)

--- pem-api/pem-rest/src/test/java/com/precisely/pem/controller/ActivityControllerTest.java
+++ pem-api/pem-rest/src/test/java/com/precisely/pem/controller/ActivityControllerTest.java
@@ -1,70 +1,75 @@
-//package com.precisely.pem.controller;
-//
-//import com.precisely.pem.commonUtil.Application;
-//import com.precisely.pem.commonUtil.SortBy;
-//import com.precisely.pem.commonUtil.SortDirection;
-//import com.precisely.pem.commonUtil.Status;
-//import com.precisely.pem.dtos.responses.ActivityDefnListResp;
-//import com.precisely.pem.dtos.responses.ActivityDefnPaginationRes;
-//import com.precisely.pem.dtos.responses.ActivityDefnResp;
-//import com.precisely.pem.services.ActivityDefnService;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.Mockito;
-//import org.mockito.MockitoAnnotations;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.mock.web.MockMultipartFile;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//import static org.junit.jupiter.api.Assertions.assertNotNull;
-//
-//class ActivityControllerTest {
-//    @InjectMocks
-//    ActivityController activityController;
-//
-//    @Mock
-//    ActivityDefnService activityDefnService;
-//
-//    @BeforeEach
-//    void setup() throws Exception{
-//        MockitoAnnotations.openMocks(this);
-//    }
-//    @Test
-//    void testCreateActivityDefinition() throws Exception {
-//        String sponsorContext = "test";
-//        String name = "test";
-//        String description = "test";
-//        String application = "PEM";
-//        MultipartFile file = new MockMultipartFile("file", "test.txt", "text/plain", "This is a test file.".getBytes());
-//
-//        ActivityDefnResp resp = new ActivityDefnResp();
-//        Mockito.when(activityDefnService.createActivityDefinition(Mockito.anyString(),Mockito.anyString(),Mockito.anyString(),Mockito.any(MultipartFile.class),Mockito.anyString()))
-//                .thenReturn(resp);
-//        ResponseEntity<Object> output = activityController.createActivityDefinition(name,description,file, Application.PEM,sponsorContext);
-//        assertNotNull(output);
-//    }
-//
-//    @Test
-//    void testGetActivityDefinitionList() throws Exception {
-//        String sponsorContext = "test";
-//        String name = "test";
-//        ActivityDefnPaginationRes resp = new ActivityDefnPaginationRes();
-//        Mockito.when(activityDefnService.getAllDefinitionList(Mockito.anyString(),Mockito.anyString(),Mockito.anyString(),Mockito.anyString(),Mockito.anyString(),Mockito.anyInt(),Mockito.anyInt(),Mockito.anyString(),Mockito.anyString()))
-//                .thenReturn(resp);
-//        ResponseEntity<Object> output = activityController.getActivityDefinitionList(sponsorContext,name, Status.DRAFT,Application.PEM,1,1, SortBy.modify_ts, SortDirection.ASC,"cashbank");
-//        assertNotNull(output);
-//    }
-//
-//    @Test
-//    void testGetActivityDefinitionById() throws Exception {
-//        String sponsorContext = "test";
-//        String activityDefnKey = "test";
-//        ActivityDefnListResp resp = new ActivityDefnListResp();
-//        Mockito.when(activityDefnService.getActivityDefinitionByKey(Mockito.anyString(),Mockito.anyString()))
-//                .thenReturn(resp);
-//        ResponseEntity<Object> output = activityController.getActivityDefinitionByKey(sponsorContext,activityDefnKey);
-//        assertNotNull(output);
-//    }
-//}
+package com.precisely.pem.controller;
+
+import com.precisely.pem.commonUtil.Application;
+import com.precisely.pem.commonUtil.SortBy;
+import com.precisely.pem.commonUtil.SortDirection;
+import com.precisely.pem.commonUtil.Status;
+import com.precisely.pem.dtos.responses.ActivityDefnListResp;
+import com.precisely.pem.dtos.responses.ActivityDefnPaginationRes;
+import com.precisely.pem.dtos.responses.ActivityDefnResp;
+import com.precisely.pem.services.ActivityDefnService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class ActivityControllerTest {
+    @InjectMocks
+    ActivityController activityController;
+
+    @Mock
+    ActivityDefnService activityDefnService;
+
+    @BeforeEach
+    void setup() throws Exception{
+        MockitoAnnotations.openMocks(this);
+    }
+    @Test
+    void testCreateActivityDefinition() throws Exception {
+        String sponsorContext = "test";
+        String name = "test";
+        String description = "test";
+        String application = "PEM";
+        MultipartFile file = new MockMultipartFile("file", "test.txt", "text/plain", "This is a test file.".getBytes());
+
+        ActivityDefnResp resp = new ActivityDefnResp();
+        Mockito.when(activityDefnService.createActivityDefinition(Mockito.anyString(),Mockito.anyString(),Mockito.anyString(),Mockito.any(MultipartFile.class),Mockito.anyString()))
+                .thenReturn(resp);
+        ResponseEntity<Object> output = activityController.createActivityDefinition(name,description,file, Application.PEM,sponsorContext);
+        assertNotNull(output);
+    }
+
+    @Test
+    void testGetActivityDefinitionList() throws Exception {
+        String sponsorContext = "test";
+        String name = "test";
+        ActivityDefnPaginationRes resp = new ActivityDefnPaginationRes();
+        List<ActivityDefnListResp> listResp = new ArrayList<>();
+        resp.setContent(listResp);
+        Mockito.when(activityDefnService.getAllDefinitionList(Mockito.anyString(),Mockito.anyString(),Mockito.anyString(),Mockito.anyString(),Mockito.anyString(),Mockito.anyInt(),Mockito.anyInt(),Mockito.anyString(),Mockito.anyString()))
+                .thenReturn(resp);
+        ResponseEntity<Object> output = activityController.getActivityDefinitionList(sponsorContext,name, Status.DRAFT,Application.PEM,1,1, SortBy.modify_ts, SortDirection.ASC,"cashbank");
+        assertNotNull(output);
+    }
+
+    @Test
+    void testGetActivityDefinitionById() throws Exception {
+        String sponsorContext = "test";
+        String activityDefnKey = "test";
+        ActivityDefnListResp resp = new ActivityDefnListResp();
+        Mockito.when(activityDefnService.getActivityDefinitionByKey(Mockito.anyString(),Mockito.anyString()))
+                .thenReturn(resp);
+        ResponseEntity<Object> output = activityController.getActivityDefinitionByKey(sponsorContext,activityDefnKey);
+        assertNotNull(output);
+    }
+}
--- pem-api/pem-rest/src/test/java/com/precisely/pem/controller/ActivityVersionControllerTest.java
+++ pem-api/pem-rest/src/test/java/com/precisely/pem/controller/ActivityVersionControllerTest.java
@@ -1,63 +1,66 @@
-//package com.precisely.pem.controller;
-//
-//import com.precisely.pem.commonUtil.Application;
-//import com.precisely.pem.commonUtil.SortBy;
-//import com.precisely.pem.commonUtil.SortDirection;
-//import com.precisely.pem.commonUtil.Status;
-//import com.precisely.pem.exceptionhandler.OnlyOneDraftVersionException;
-//import com.precisely.pem.services.ActivityVersionService;
-//import jakarta.servlet.http.HttpServletRequest;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.Mockito;
-//import org.mockito.MockitoAnnotations;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.mock.web.MockMultipartFile;
-//import org.springframework.web.multipart.MultipartFile;
-//
-//import java.io.IOException;
-//import java.sql.SQLException;
-//
-//import static org.junit.jupiter.api.Assertions.assertNotNull;
-//
-//class ActivityVersionControllerTest {
-//    @InjectMocks
-//    ActivityVersionController activityVersionController;
-//    @Mock
-//    ActivityVersionService activityVersionService;
-//    @BeforeEach
-//    void setup() throws Exception{
-//        MockitoAnnotations.openMocks(this);
-//    }
-//    @Mock
-//    HttpServletRequest req;
-//    @Test
-//    void testGetActivityDefinitionVersionsList() {
-//        ResponseEntity<Object> resp = ResponseEntity.ok().build();
-//        Mockito.when(activityVersionService.getAllVersionDefinitionList(Mockito.anyString(),Mockito.anyString(),Mockito.anyString(),Mockito.anyBoolean(),Mockito.anyInt(),Mockito.anyInt(),Mockito.anyString(),Mockito.anyString(),Mockito.anyString()))
-//                .thenReturn(resp);
-//        ResponseEntity<Object> output = activityVersionController.getActivityVersionDefinitionList("test",false,"test", Status.DRAFT,0,1, SortBy.modify_ts, SortDirection.ASC,"cashbank");
-//        assertNotNull(output);
-//    }
-//
-//    @Test
-//    void testGetActivityDefinitionVersionById() throws Exception {
-//        ResponseEntity<Object> resp = ResponseEntity.ok().build();
-//        Mockito.when(activityVersionService.getVersionDefinitionById(Mockito.anyString(),Mockito.anyString(),Mockito.anyDouble()))
-//                .thenReturn(resp);
-//        ResponseEntity<Object> output = activityVersionController.getActivityVersionDefinitionById("test",1.0,"test");
-//        assertNotNull(output);
-//    }
-//
-//    @Test
-//    void testPostCreateActivityDefnVersion() throws SQLException, IOException, OnlyOneDraftVersionException {
-//        MultipartFile file = new MockMultipartFile("file", "test.txt", "text/plain", "This is a test file.".getBytes());
-//        ResponseEntity<Object> resp = ResponseEntity.ok().build();
-//        Mockito.when(activityVersionService.createActivityDefnVersion(Mockito.anyString(),Mockito.anyString(),Mockito.any(MultipartFile.class),Mockito.anyBoolean(),Mockito.anyString()))
-//                .thenReturn(resp);
-//        ResponseEntity<Object> output = activityVersionController.createActivityDefinition("test","test",file,false, Application.PEM,req);
-//        assertNotNull(output);
-//    }
-//}
+package com.precisely.pem.controller;
+
+import com.precisely.pem.commonUtil.Application;
+import com.precisely.pem.commonUtil.SortBy;
+import com.precisely.pem.commonUtil.SortDirection;
+import com.precisely.pem.commonUtil.Status;
+import com.precisely.pem.dtos.responses.ActivityDefnVersionResp;
+import com.precisely.pem.dtos.responses.ActivityVersionDefnPaginationResp;
+import com.precisely.pem.dtos.shared.ActivityDefnVersionDto;
+import com.precisely.pem.exceptionhandler.OnlyOneDraftVersionException;
+import com.precisely.pem.services.ActivityVersionService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class ActivityVersionControllerTest {
+    @InjectMocks
+    ActivityVersionController activityVersionController;
+    @Mock
+    ActivityVersionService activityVersionService;
+    @BeforeEach
+    void setup() throws Exception{
+        MockitoAnnotations.openMocks(this);
+    }
+    @Mock
+    HttpServletRequest req;
+    @Test
+    void testGetActivityDefinitionVersionsList() throws Exception {
+        ActivityVersionDefnPaginationResp resp = new ActivityVersionDefnPaginationResp();
+        Mockito.when(activityVersionService.getAllVersionDefinitionList(Mockito.anyString(),Mockito.anyString(),Mockito.anyString(),Mockito.anyBoolean(),Mockito.anyInt(),Mockito.anyInt(),Mockito.anyString(),Mockito.anyString(),Mockito.anyString()))
+                .thenReturn(resp);
+        ResponseEntity<Object> output = activityVersionController.getActivityVersionDefinitionList("test",false,"test", Status.DRAFT,0,1, SortBy.modify_ts, SortDirection.ASC,"cashbank");
+        assertNotNull(output);
+    }
+
+    @Test
+    void testGetActivityDefinitionVersionById() throws Exception {
+        ActivityDefnVersionDto resp = new ActivityDefnVersionDto();
+        Mockito.when(activityVersionService.getVersionDefinitionById(Mockito.anyString(),Mockito.anyString(),Mockito.anyDouble()))
+                .thenReturn(resp);
+        ResponseEntity<Object> output = activityVersionController.getActivityVersionDefinitionById("test",1.0,"test");
+        assertNotNull(output);
+    }
+
+    @Test
+    void testPostCreateActivityDefnVersion() throws SQLException, IOException, OnlyOneDraftVersionException {
+        MultipartFile file = new MockMultipartFile("file", "test.txt", "text/plain", "This is a test file.".getBytes());
+        ActivityDefnVersionResp resp = new ActivityDefnVersionResp();
+        Mockito.when(activityVersionService.createActivityDefnVersion(Mockito.anyString(),Mockito.anyString(),Mockito.any(MultipartFile.class),Mockito.anyBoolean(),Mockito.anyString()))
+                .thenReturn(resp);
+        ResponseEntity<Object> output = activityVersionController.createActivityDefinition("test","test",file,false, Application.PEM);
+        assertNotNull(output);
+    }
+}

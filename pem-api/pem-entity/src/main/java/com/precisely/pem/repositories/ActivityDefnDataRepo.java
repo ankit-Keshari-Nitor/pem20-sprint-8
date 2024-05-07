11ae8321dce874e949b33fe48c09dea07ae2304d
yogesh.sharma <yogesh.sharma@precisely.com>
2024-05-07 18:41:47 +0530

    unused import remove

--- pem-api/pem-entity/src/main/java/com/precisely/pem/repositories/ActivityDefnDataRepo.java
+++ pem-api/pem-entity/src/main/java/com/precisely/pem/repositories/ActivityDefnDataRepo.java
@@ -2,13 +2,7 @@ package com.precisely.pem.repositories;
 
 import com.precisely.pem.models.ActivityDefnData;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Repository
 public interface ActivityDefnDataRepo extends JpaRepository<ActivityDefnData,String> {

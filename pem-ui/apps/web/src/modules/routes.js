6198df80d2b659ebb526ebed5385251ab561a683
Pradeep K <Pradeep.K@precisely.com>
2024-05-20 19:04:34 +0530

    PEM-27026: Pem UI build fix

--- pem-ui/apps/web/src/modules/routes.js
+++ pem-ui/apps/web/src/modules/routes.js
@@ -1,4 +1,3 @@
-import React from 'react';
 import { routes as PartnerRoutes } from './partner';
 // import { routes as SponsorUserRoutes } from './sponsor-users';
 import { routes as DesignerRoutes } from './designer';

fb00228352329d3509e61b2aec427ae737fc0aff
Anshudha Maheshwari <anshudha.maheshwari@precisely.com>
2024-06-03 13:39:00 +0530

    Added the test case for carbon Mapper - File-upload, text, toggle, label

---
+++ pem-ui/packages/carbon-mappers/src/form-fields/__tests__/label.test.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Label from '../label';
+
+describe('Label', () => {
+    it('renders label text and style correctly', () => {
+        const labelText = 'Test Label';
+        const isRequired = true;
+
+        const { getByText, container } = render(
+            <Label labelText={labelText} isRequired={isRequired} />
+        );
+
+        const labelElement = getByText(labelText);
+        expect(labelElement).toBeInTheDocument();
+        expect(labelElement).toHaveStyle('color: red');
+        const asteriskElement = container.querySelector('span > span');
+        if (isRequired) {
+            expect(asteriskElement).toBeInTheDocument();
+            expect(asteriskElement.textContent).toBe('*');
+        } else {
+            expect(asteriskElement).not.toBeInTheDocument();
+        }
+    });
+
+    it('renders label text without asterisk if isRequired is false', () => {
+        const labelText = 'Test Label';
+        const isRequired = false;
+
+        const { getByText, container } = render(
+            <Label labelText={labelText} isRequired={isRequired} />
+        );
+
+        const labelElement = getByText(labelText);
+        expect(labelElement).toBeInTheDocument();
+
+        expect(labelElement).toHaveStyle('color: #525252');
+        const asteriskElement = container.querySelector('span > span');
+        expect(asteriskElement).not.toBeInTheDocument();
+    });
+});

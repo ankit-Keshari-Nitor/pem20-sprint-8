35c72b489811be6ad66cfd9244aba8d6a7e18e10
yogesh.sharma <yogesh.sharma@precisely.com>
2024-06-12 15:15:17 +0530

    remove unnecessary constants

--- pem-api/pem-activiti/src/main/java/com/precisely/pem/dtos/Constants.java
+++ pem-api/pem-activiti/src/main/java/com/precisely/pem/dtos/Constants.java
@@ -6,31 +6,4 @@ public class Constants {
     public static final int TASK_WIDTH = 200;
     public static final int TASK_HEIGHT =  200;
     public static final String PEM_PROCESS_ID = "PemProcess";
-
-    //TODO temporary constants, need to be removed.
-    public static final String USER_NODE_RESOURCE_ID = "FormNode_1";
-    public static final String END_NODE_RESOURCE_ID = "EndNode_1";
-    public static final String SUB_END_NODE_RESOURCE_ID = "SubEndNode_1";
-    public static final String SUB_SUB_END_NODE_RESOURCE_ID = "SubSubEndNode_1";
-    public static final String SUB_PROCESS_1 = "SubProcess_1";
-    public static final String SUB_SUB_PROCESS_2 = "SubSubProcess_2";
-    public static final String END_NODE_RESOURCE_ID_2 = "EndNode_2";
-    public static final String START_NODE_RESOURCE_ID = "StartNode_1";
-    public static final String SUB_START_NODE_RESOURCE_ID = "SubStartNode_1";
-    public static final String SUB_SUB_START_NODE_RESOURCE_ID = "SubSubStartNode_1";
-    public static final String API_NODE_1 = "apiNode1";
-    public static final String API_NODE_2 = "apiNode2";
-    public static final String XSLT_NODE_1 = "xsltNode1";
-    public static final String GATEWAY_1 = "gateway1";
-    public static final String SEQUENCE_FLOW_RESOURCE_ID_1 = "SequenceFlow1";
-    public static final String SEQUENCE_FLOW_RESOURCE_ID_2 = "SequenceFlow2";
-    public static final String SEQUENCE_FLOW_RESOURCE_ID_3 = "SequenceFlow3";
-    public static final String SEQUENCE_FLOW_RESOURCE_ID_4 = "SequenceFlow4";
-    public static final String SEQUENCE_FLOW_RESOURCE_ID_5 = "SequenceFlow5";
-    public static final String SEQUENCE_FLOW_RESOURCE_ID_6 = "SequenceFlow6";
-    public static final String SEQUENCE_FLOW_RESOURCE_ID_7 = "SequenceFlow7";
-    public static final String SEQUENCE_FLOW_RESOURCE_ID_8 = "SequenceFlow8";
-    public static final String SEQUENCE_FLOW_RESOURCE_ID_9 = "SequenceFlow9";
-    public static final String SEQUENCE_FLOW_RESOURCE_ID_10 = "SequenceFlow10";
-    public static final String SEQUENCE_FLOW_RESOURCE_ID_11 = "SequenceFlow11";
 }

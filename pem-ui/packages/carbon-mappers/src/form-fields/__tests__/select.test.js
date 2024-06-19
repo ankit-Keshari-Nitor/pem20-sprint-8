a18b85256eaecbc8ae7f5e14c8efbd0393959dfa
Anshudha Maheshwari <anshudha.maheshwari@precisely.com>
2024-06-19 11:52:00 +0530

    Added the test cases for select

--- pem-ui/packages/carbon-mappers/src/form-fields/__tests__/select.test.js
+++ pem-ui/packages/carbon-mappers/src/form-fields/__tests__/select.test.js
@@ -1,28 +1,38 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import Select from '../select'; // Adjust the import path as necessary
-import { FORM_FIELD_TYPE } from '../../constant';
+import { render, fireEvent } from '@testing-library/react';
+import Select from '../select';
 
-const mockField = {
-  type: FORM_FIELD_TYPE.SELECT,
+// Mock data for testing
+const field = {
   labelText: 'Test Label',
   isRequired: true,
   options: [
-    { id: '1', value: 'option1', label: 'Option 1' },
-    { id: '2', value: 'option2', label: 'Option 2' }
+    { id: 1, value: 'option1', label: 'Option 1' },
+    { id: 2, value: 'option2', label: 'Option 2' },
+    { id: 3, value: 'option3', label: 'Option 3' },
   ],
-  isDisabled: false,
-  readOnly: false
 };
 
-describe('Select Component', () => {
-  it('renders the select component with the correct label and options', () => {
-    render(<Select field={mockField} id="test-select" />);
+describe('Select component', () => {
+  it('renders with options correctly', () => {
+    const { getByLabelText, getByText } = render(<Select field={field} id="test-select" />);
 
-    expect(screen.getByLabelText('Test Label')).toBeInTheDocument();
-
-    mockField.options.forEach((option) => {
-      expect(screen.getByText(option.label)).toBeInTheDocument();
-    });
+    expect(getByLabelText('Test Label')).toBeInTheDocument();
+    expect(getByText('Option 1')).toBeInTheDocument();
+    expect(getByText('Option 2')).toBeInTheDocument();
+    expect(getByText('Option 3')).toBeInTheDocument();
   });
+
+  it('renders without options when options are not provided', () => {
+    const fieldWithoutOptions = { ...field, options: undefined };
+    const { getByText, queryByText } = render(<Select field={fieldWithoutOptions} id="test-select" />);
+
+    expect(getByText('Test Label')).toBeInTheDocument();
+
+    expect(queryByText('Option 1')).toBeNull();
+    expect(queryByText('Option 2')).toBeNull();
+    expect(queryByText('Option 3')).toBeNull();
+  });
+
+
 });

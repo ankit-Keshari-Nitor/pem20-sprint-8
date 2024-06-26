6944d76b2aa0b00ba3515bc49cae624fcba1deb0
yogesh.sharma <yogesh.sharma@precisely.com>
2024-06-26 16:40:54 +0530

    refactor processId to append always ID-

--- pem-api/pem-activiti/src/main/java/com/precisely/pem/dtos/BpmnConverterRequest.java
+++ pem-api/pem-activiti/src/main/java/com/precisely/pem/dtos/BpmnConverterRequest.java
@@ -9,11 +9,31 @@ import java.util.Map;
 
 
 @Data
-@Builder
 public class BpmnConverterRequest {
     Map<String, List<String>> sourceMap;
     Map<String, Connector> connectorsMap;
     BpmnModel bpmnModel;
-    String processId;
+    private final String processId;
+
+    private BpmnConverterRequest(Builder builder) {
+        this.processId = builder.processId;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private String processId;
+
+        public Builder processId(String processId) {
+            this.processId = "ID-" + processId;
+            return this;
+        }
+
+        public BpmnConverterRequest build() {
+            return new BpmnConverterRequest(this);
+        }
+    }
 
 }

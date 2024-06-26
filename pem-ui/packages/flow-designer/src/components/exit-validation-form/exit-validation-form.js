85800c1a5ba751dfacecda153a97067499855cb6
Ankit Kesari <Ankit.Kesari@precisely.com>
2024-06-27 05:21:46 +0530

    Updated Exit Validation Form

--- pem-ui/packages/flow-designer/src/components/exit-validation-form/exit-validation-form.js
+++ pem-ui/packages/flow-designer/src/components/exit-validation-form/exit-validation-form.js
@@ -1,33 +1,53 @@
 import React, { useState } from 'react';
-import { TextArea, Grid, Column } from '@carbon/react';
-import './exit-validation-form.scss';
-import CarbonWrapper from './condition-builder-wrapper/carbon-wrapper';
-import QueryBuilder, { defaultOperators, formatQuery } from 'react-querybuilder';
-import { QueryBuilderDnD } from '@react-querybuilder/dnd';
 import * as ReactDnD from 'react-dnd';
+import { TextArea, Grid, Column, Button } from '@carbon/react';
+import { QueryBuilderDnD } from '@react-querybuilder/dnd';
 import * as ReactDndHtml5Backend from 'react-dnd-html5-backend';
+import CarbonWrapper from './condition-builder-wrapper/carbon-wrapper';
+import QueryBuilder, { formatQuery } from 'react-querybuilder';
+import { validationQuery } from '../helpers/generate-validation-query';
 import { INITIAL_QUERY, QUERY_COMBINATOR, QUERY_FIELDS } from '../../constants';
 
-export default function ExitValidationFrom() {
+import './exit-validation-form.scss';
+
+export default function ExitValidationFrom({ onSubmitExitValidationForm, setOpenCancelDialog }) {
   const [query, setQuery] = useState(INITIAL_QUERY);
   return (
-    <div className="query-builder">
-      <CarbonWrapper>
-        <QueryBuilderDnD dnd={{ ...ReactDnD, ...ReactDndHtml5Backend }}>
-          <QueryBuilder
-            fields={QUERY_FIELDS}
-            query={query}
-            onQueryChange={setQuery}
-            combinators={QUERY_COMBINATOR}
-            controlClassnames={{ queryBuilder: 'queryBuilder-branches', body: 'inline-indycomb-left' }}
-          />
-        </QueryBuilderDnD>
-      </CarbonWrapper>
-      <Grid className="grid-margin grid-margin-top">
-        <Column lg={16}>
+    <>
+      <Grid>
+        <Column className="form-field" lg={16}>
+          <CarbonWrapper>
+            <QueryBuilderDnD dnd={{ ...ReactDnD, ...ReactDndHtml5Backend }}>
+              <QueryBuilder
+                fields={QUERY_FIELDS}
+                query={query}
+                onQueryChange={setQuery}
+                combinators={QUERY_COMBINATOR}
+                controlClassnames={{ queryBuilder: 'queryBuilder-branches', body: 'inline-indycomb-left' }}
+              />
+            </QueryBuilderDnD>
+          </CarbonWrapper>
+        </Column>
+        <Column className="form-field" lg={16}>
           <TextArea placeholder="Enter Text" labelText="Error Message" rows={4} id="text-area-1" />
         </Column>
+        <Column className="form-field" lg={16}>
+          <h4>Query</h4>
+          <pre>
+            <code>{formatQuery(validationQuery(query), 'json')}</code>
+          </pre>
+        </Column>
+        <Column lg={8}>
+          <Button data-testid="cancel" name="cancel" kind="secondary" type="button" className="cancel-button" onClick={setOpenCancelDialog}>
+            Cancel
+          </Button>
+        </Column>
+        <Column lg={8}>
+          <Button data-testid="save" color="primary" variant="contained" type="submit" style={{ width: '100%' }} onClick={() => onSubmitExitValidationForm(query)}>
+            Save
+          </Button>
+        </Column>
       </Grid>
-    </div>
+    </>
   );
 }

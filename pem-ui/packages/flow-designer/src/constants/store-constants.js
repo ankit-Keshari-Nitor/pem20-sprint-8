7bf0cc46a59812d807d0a4fd8eb78015e119570f
Ankit Kesari <Ankit.Kesari@precisely.com>
2024-07-01 14:58:07 +0530

    Circular Dependency issue

--- pem-ui/packages/flow-designer/src/constants/store-constants.js
+++ pem-ui/packages/flow-designer/src/constants/store-constants.js
@@ -1,3 +1,19 @@
+export const NODE_TYPE = {
+  START: 'start',
+  END: 'end',
+  PARTNER: 'partner',
+  APPROVAL: 'approval',
+  ATTRIBUTE: 'attribute',
+  SPONSOR: 'sponsor',
+  CUSTOM: 'custom',
+  SYSTEM: 'system',
+  GATEWAY: 'gateway',
+  DIALOG: 'form',
+  XSLT: 'xslt',
+  API: 'api'
+};
+
+
 export const TASK_INITIAL_NODES = [
   {
     id: 'start',
